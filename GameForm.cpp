d7b225a79c877bf959c2ac56806c707de5f9b63a
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2025-01-26 16:50:17 +0200

    first commit

---
+++ GameForm.cpp
@@ -0,0 +1,13 @@
+#include "GameForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	Project2Tanks::GameForm form;
+	Application::Run(% form);
+}
